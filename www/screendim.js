48f25757376258ba809f04617ba118ab784e2c0c
lcnvdl <programando.soft@gmail.com>
2014-08-26 00:03:49 -0300

    Compatibility with "browser" platform
    Compatibility with "browser" platform

--- www/screendim.js
+++ www/screendim.js
@@ -1,24 +1,39 @@
-(function (gap) {
+(function (gap, w) {
     // dims by default
     var on = true;
+    
+    var screenDim = { };
+    
+    if(gap) {
 
-    gap.screenDim = { };
+        screenDim.enable = function () {
+            on = true;
+            gap.exec(null, null, 'ScreenDim', 'enable', []);
+        };
 
-    gap.screenDim.enable = function () {
-        on = true;
-        gap.exec(null, null, 'ScreenDim', 'enable', []);
-    };
+        screenDim.disable = function () {
+            on = false;
+            gap.exec(null, null, 'ScreenDim', 'disable', []);
+        };
 
-    gap.screenDim.disable = function () {
-        on = false;
-        gap.exec(null, null, 'ScreenDim', 'disable', []);
-    };
-
-    gap.screenDim.toggle = function () {
-        if (on) {
-            this.disable();
-        } else {
-            this.enable();
-        }
-    };
-}).call(this, (window.cordova || window.Cordova));
+        screenDim.toggle = function () {
+            if (on) {
+                this.disable();
+            } else {
+                this.enable();
+            }
+        };
+        
+        gap.screenDim = screenDim;
+    }
+    else {
+        screenDim.enable = function(){};
+        screenDim.disable = screenDim.enable;
+        screenDim.toggle = screenDim.enable;
+    }
+    
+    if(!w.screenDim) {
+        w.screenDim = screenDim;
+    }
+    
+}).call(this, (window.cordova || window.Cordova), window);
