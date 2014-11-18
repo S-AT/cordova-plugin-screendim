75a6907b7e17b4dc96a262072ec283722885bde0
Daniele <daniele.ferro@crmvillage.biz>
2014-11-18 16:45:53 +0100

    Update ScreenDim.m

--- src/ios/ScreenDim.m
+++ src/ios/ScreenDim.m
@@ -1,1 +1,26 @@
-y
+//
+//  ScreenDim.m
+//  ios-cordova
+//
+//  Created by Andrew Lunny on 12-07-11.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "ScreenDim.h"
+
+@implementation ScreenDim
+
+- (void) enable:(NSMutableArray*)arguments withDict:(NSMutableDictionary*)options
+{	
+    [[UIApplication sharedApplication] setIdleTimerDisabled:NO];
+    NSLog(@" ScreenDim Plugin: dim enabled");
+}
+
+
+- (void) disable:(NSMutableArray*)arguments withDict:(NSMutableDictionary*)options 
+{   
+    [[UIApplication sharedApplication] setIdleTimerDisabled:YES];
+    NSLog(@" ScreenDim Plugin: dim disabled");
+}
+
+@end
