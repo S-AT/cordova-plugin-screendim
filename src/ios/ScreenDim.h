4ec244c471e68669b6d7107861b92599af4511f2
Daniele <daniele.ferro@crmvillage.biz>
2014-11-18 16:45:14 +0100

    Update ScreenDim.h

--- src/ios/ScreenDim.h
+++ src/ios/ScreenDim.h
@@ -1,1 +1,17 @@
-x
+//
+//  ScreenDim.h
+//  ios-cordova
+//
+//  Created by Andrew Lunny on 12-07-11.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <Cordova/CDVPlugin.h>
+
+@interface ScreenDim : CDVPlugin
+
+- (void) enable:(NSMutableArray*)arguments withDict:(NSMutableDictionary*)options;
+- (void) disable:(NSMutableArray*)arguments withDict:(NSMutableDictionary*)options;
+
+@end
